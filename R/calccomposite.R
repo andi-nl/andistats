a4fc6fcce85945b10405e4f47aea04f094eddb8d
JAvRZ <joostzandvliet@gmail.com>
2016-05-31 11:43:21 +0200

    Rework list of function definitions
    This new version uses actual variable names, is cleaner and clearer. Maybe the _func part is unnecessary, and OpenCPU can just call the function from the list. The bottom part consists of examples of current functionality (is this what we want) that should be removed before actual implementation.

--- R/calccomposite.R
+++ R/calccomposite.R
@@ -1,21 +1,37 @@
 ##### These can all be preloaded in the sysdata.rda file I think #####
-calccomposite <- function( compositename = "", v1, v2, v3, v4, v5){
-  return( do.call( composite.list$func[composite.list$name == compositename], composite.list$args))
-}
 
-AVLTsum1to5.func <- function( v1, v2, v3, v4, v5, ...){ return( v1 + v2 + v3 + v4 + v5 )}
-TMTint.func <- function( v1, v2, ...){ return( v2 - v1 )}
-
-composite.list <- list( name = c("AVLTtotal1-5", "TMTinterference"),
-                        func = c("AVLTsum1to5.func", "TMTint.func"),
-                        args = list(as.name("v1"), as.name("v2"), as.name("v3"), as.name("v4"), as.name("v5")))
-######################################################################
+calccomposite <-
+      list(
+          "AVLTtotal1-5" = function(AVLT_trial1, AVLT_trial2, AVLT_trial3, AVLT_trial4, AVLT_trial5, ...) {
+            return(AVLT_trial1 + AVLT_trial2 + AVLT_trial3 + AVLT_trial4 + AVLT_trial5)
+            },
+          "TMTinterference" = function(TMTa, TMTb, ...) {
+            return(TMTb - TMTa)
+            }
+          )
 
 # These are example calls to OpenCPU that return the appropriate values
-calccomposite( "AVLTtotal1-5", 5, 7, 8, 15, 15)
-calccomposite( "TMTinterference", 60, 180)
+calccomposite[["AVLTtotal1-5"]]( 5, 7, 8, 15, 15)
+calccomposite[["TMTinterference"]]( 60, 180)
 
-# And in the CSV, we'll add a column named something like consistuents, or args, or whatever
+
+
+# This might already be enough. But, there may be advantages to calling a function.
+calccomposite_func <- function( testname, ...){
+  calccomposite[[testname]]( ...)
+}
+
+# This works correctly (returns 37)
+calccomposite_func( "AVLTtotal1-5", AVLT_trial1 = 5, AVLT_trial2 = 5, AVLT_trial3 = 7, AVLT_trial4 = 8 , AVLT_trial5 = 12)
+calccomposite_func( "AVLTtotal1-5", 5, 5, 7, 8, 12)
+
+# This correctly doesn't work (returns argument "TMTb" is missing)
+calccomposite_func( "TMTinterference", AVLT_trial1 = 5, AVLT_trial2 = 5, AVLT_trial3 = 7, AVLT_trial4 = 8 , AVLT_trial5 = 12)
+
+# This works, but this might indicate  that it's too flexible
+calccomposite_func( "TMTinterference", AVLT_trial1 = 5, 60, AVLT_trial2 = 5, AVLT_trial3 = 7, AVLT_trial4 = 8 , AVLT_trial5 = 12, 180)
+
+# And in the CSV, we'll add a column named something like constituents, or args, or whatever
 # which contains AVLT_trial1,AVLT_trial2,AVLT_trial3,AVLT_trial4,AVLT_trial5 for the AVLTtotal1-5 row
 # and contains TMT_a,TMT_b for the TMTinterference row.
 
