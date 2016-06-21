94e7447df2f587f9212d831dc9214ad7f5c5acb0
JAvRZ <joostzandvliet@gmail.com>
2016-06-21 14:43:35 +0200

    Remove examples, document, update
    Added roxygen skeleton with some info, and there were a lot of examples in the text, that are more suitable for the documentation. Also replaced separators "-" with "__".

--- R/calccomposite.R
+++ R/calccomposite.R
@@ -1,40 +1,33 @@
-##### These can all be preloaded in the sysdata.rda file I think #####
-
-calccomposite <-
+listcompositecalculatons <-
       list(
-          "AVLTtotal1-5" = function(AVLT_trial1, AVLT_trial2, AVLT_trial3, AVLT_trial4, AVLT_trial5, ...) {
-            return(AVLT_trial1 + AVLT_trial2 + AVLT_trial3 + AVLT_trial4 + AVLT_trial5)
+          "AVLT__total1_to_5" = function(AVLT__trial1, AVLT__trial2, AVLT__trial3, AVLT__trial4, AVLT__trial5, ...) {
+            return(AVLT__trial1 + AVLT__trial2 + AVLT__trial3 + AVLT__trial4 + AVLT__trial5)
             },
-          "TMTinterference" = function(TMTa, TMTb, ...) {
-            return(TMTb - TMTa)
-            }
+          "TMT__interference" = function(TMTa, TMTb, ...) {
+            return(TMT__b - TMT__a)
+          }
+          "BADS__ruleshift__1and2scorediff" = function( BADS__ruleshift__1score, BADS__ruleshift__2score){
+            return(BADS__ruleshift__2score - BADS__ruleshift__1score)
+          }
+          "BADS__ruleshift__1and2timediff" = function( BADS__ruleshift__1time, BADS__ruleshift__2time){
+            return(BADS__ruleshift__2time - BADS__ruleshift__1time)
+          }
           )
 
-# These are example calls to OpenCPU that return the appropriate values
-calccomposite[["AVLTtotal1-5"]]( 5, 7, 8, 15, 15)
-calccomposite[["TMTinterference"]]( 60, 180)
+#' Title Calculate composite score using lists of functions
+#'
+#' @param testname Name of the composite score
+#' @param ... Arguments are substituent test scores that are needed in the calculation of the test score. May be named or unnamed (if named, names have to be correct)
+#'
+#' @return Value of the composite score
+#' @export
+#'
+#' @examples calccomposite_func( "AVLT__total_1_to_5", AVLT_trial1 = 5, AVLT_trial2 = 5, AVLT_trial3 = 7, AVLT_trial4 = 8 , AVLT_trial5 = 12)
+#' @examples calccomposite_func( "AVLT__total_1_to_5", 5, 5, 7, 8, 12)
 
-
-
-# This might already be enough. But, there may be advantages to calling a function.
 calccomposite_func <- function( testname, ...){
   calccomposite[[testname]]( ...)
 }
-
-# This works correctly (returns 37)
-calccomposite_func( "AVLTtotal1-5", AVLT_trial1 = 5, AVLT_trial2 = 5, AVLT_trial3 = 7, AVLT_trial4 = 8 , AVLT_trial5 = 12)
-calccomposite_func( "AVLTtotal1-5", 5, 5, 7, 8, 12)
-
-# This correctly doesn't work (returns argument "TMTb" is missing)
-calccomposite_func( "TMTinterference", AVLT_trial1 = 5, AVLT_trial2 = 5, AVLT_trial3 = 7, AVLT_trial4 = 8 , AVLT_trial5 = 12)
-
-# This works, but this might indicate  that it's too flexible
-calccomposite_func( "TMTinterference", AVLT_trial1 = 5, 60, AVLT_trial2 = 5, AVLT_trial3 = 7, AVLT_trial4 = 8 , AVLT_trial5 = 12, 180)
-
-# And in the CSV, we'll add a column named something like constituents, or args, or whatever
-# which contains AVLT_trial1,AVLT_trial2,AVLT_trial3,AVLT_trial4,AVLT_trial5 for the AVLTtotal1-5 row
-# and contains TMT_a,TMT_b for the TMTinterference row.
-
 
 
 
