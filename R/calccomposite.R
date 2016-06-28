608cb47cff3e5198077a7e01a2a912a1cd799032
JAvRZ <joostzandvliet@gmail.com>
2016-06-28 11:58:37 +0200

    Add json (to and from) to function
    The list of functions has been moved to the static data. The calccomposite function should be dynamically called in the forms. It now takes in a json file, and gives back a json file.

--- R/calccomposite.R
+++ R/calccomposite.R
@@ -1,33 +1,16 @@
-listcompositecalculations <-
-      list(
-          "AVLT__total1_to_5" = function(AVLT__trial1, AVLT__trial2, AVLT__trial3, AVLT__trial4, AVLT__trial5, ...) {
-            return(AVLT__trial1 + AVLT__trial2 + AVLT__trial3 + AVLT__trial4 + AVLT__trial5)
-            },
-          "TMT__interference" = function(TMTa, TMTb, ...) {
-            return(TMT__b - TMT__a)
-          },
-          "BADS__ruleshift__1and2scorediff" = function( BADS__ruleshift__1score, BADS__ruleshift__2score){
-            return(BADS__ruleshift__2score - BADS__ruleshift__1score)
-          },
-          "BADS__ruleshift__1and2timediff" = function( BADS__ruleshift__1time, BADS__ruleshift__2time){
-            return(BADS__ruleshift__2time - BADS__ruleshift__1time)
-          }
-          )
-
-#' Title Calculate composite score using lists of functions
+#' Title Calculate composite score using one of many functions
 #'
-#' @param testname Name of the composite score
-#' @param ... Arguments are substituent test scores that are needed in the calculation of the test score. May be named or unnamed (if named, names have to be correct)
+#' @param inputfile JSON file that contains compVar, which is the name of the composite variable, and args, which contains the named arguments and values that go into the calculations
 #'
-#' @return Value of the composite score
+#' @return JSON formatted compositescore
+#' @importFrom jsonlite fromJSON toJSON
 #' @export
 #'
-#' @examples calccomposite_func( "AVLT__total_1_to_5", AVLT_trial1 = 5, AVLT_trial2 = 5, AVLT_trial3 = 7, AVLT_trial4 = 8 , AVLT_trial5 = 12)
-#' @examples calccomposite_func( "AVLT__total_1_to_5", 5, 5, 7, 8, 12)
+#' @examples calccomposite( "exampleJSON.json")
 
-calccomposite_func <- function( testname, ...){
-  calccomposite[[testname]]( ...)
-}
-
-
-
+calccomposite <- function( inputfile){
+  inputvals <- fromJSON(inputfile)
+  compositescore <- as.data.frame(do.call( compositefunctions[[inputvals$compVar]], inputvals$args ))
+  names(compositescore) <- inputvals$compVar
+  return(toJSON(compositescore))
+  }
